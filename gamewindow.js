6dc9cfb760e89c620359b2fa5102f4ce5c89fe8b
Ryan Jacoby <rjacoby00@icloud.com>
2017-10-23 11:44:45 -0700

    Update gamewindow.js

--- gamewindow.js
+++ gamewindow.js
@@ -1,7 +1,7 @@
 extcss = document.createElement('link');
 e = document.createElement('div');
 extcss.rel = 'stylesheet';
-extcss.href = '[LINK]';
+extcss.href = 'https://rawgit.com/rjacoby00/browsergames/master/index.css';
 document.head.appendChild(extcss);
 e.class = 'game_container_rjacoby';
 document.body.appendChild(e);
