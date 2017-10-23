ad6a773de81add4096324fd6e9e6f772e2a4beae
Ryan Jacoby <rjacoby00@icloud.com>
2017-10-23 11:41:59 -0700

    Update gamewindow.js

--- gamewindow.js
+++ gamewindow.js
@@ -1,1 +1,8 @@
-alert("test")
+extcss = document.createElement('link');
+e = document.createElement('div');
+extcss.rel = 'stylesheet';
+extcss.href = '[LINK]';
+document.head.appendChild(extcss);
+e.class = 'game_container_rjacoby';
+document.body.appendChild(e);
+document.getElementsByClass('game_container_rjacoby')[0].innerHTML = "TEST";
