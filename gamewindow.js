71791ead78074e3e8100ffaa0f186fe870b41f2e
Ryan Jacoby <rjacoby00@icloud.com>
2017-10-24 12:47:34 -0700

    Update gamewindow.js
    add use strict

--- gamewindow.js
+++ gamewindow.js
@@ -1,3 +1,4 @@
+"use strict";
 var div = document.createElement('div');
 div.setAttribute('class', 'rjacoby_game');
 div.innerHTML = '<link href="https://rawgit.com/rjacoby00/browsergames/master/index.css" rel="stylesheet" type="text/css" /><div class="rjacoby_game_container"><h1>Test</h1></div>';
