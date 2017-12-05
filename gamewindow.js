e698a6138e4c84901245b470e30636afb2fff883
Ryan Jacoby <rjacoby00@icloud.com>
2017-12-05 12:39:26 -0800

    Update gamewindow.js

--- gamewindow.js
+++ gamewindow.js
@@ -9,9 +9,10 @@ var x = canvas.width/2;
 var y = canvas.height-30;
 var dx = 2;
 var dy = -2;
+var ballRadius = 10;
 function drawBall() {
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
@@ -20,6 +21,12 @@ function drawBall() {
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
+    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+        dy = -dy;
+    }
     x += dx;
     y += dy;
 }
