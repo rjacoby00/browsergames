fac9708e9b3f6fc1f592e91c0385fb1ab30bdc1c
Ryan Jacoby <rjacoby00@icloud.com>
2017-12-05 12:50:46 -0800

    Update gamewindow.js

--- gamewindow.js
+++ gamewindow.js
@@ -3,6 +3,7 @@ var div = document.createElement('div');
 div.setAttribute('class', 'rjacoby_game');
 div.innerHTML = '<link href="https://rawgit.com/rjacoby00/browsergames/master/index.css" rel="stylesheet" type="text/css" /><canvas width="480" height="320" id="rjgamesidrj" class="rjacoby_game_container"></canvas>';
 document.body.appendChild(div);
+
 var canvas = document.getElementById("rjgamesidrj");
 var ctx = canvas.getContext("2d");
 var x = canvas.width/2;
@@ -10,14 +11,48 @@ var y = canvas.height-30;
 var dx = 2;
 var dy = -2;
 var ballRadius = 10;
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width-paddleWidth)/2;
+var color = "#0095DD";
+var rightPressed = false;
+var leftPressed = false;
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = true;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = false;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = false;
+    }
+}
+
 function drawBall() {
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-    ctx.fillStyle = "#0095DD";
+    ctx.fillStyle = color;
     ctx.fill();
     ctx.closePath();
 }
-
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = color;
+    ctx.fill();
+    ctx.closePath();
+}
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
@@ -26,6 +61,12 @@ function draw() {
     }
     if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
         dy = -dy;
+    }
+    if(rightPressed && paddleX < canvas.width-paddleWidth) {
+        paddleX += 7;
+    }
+    else if(leftPressed && paddleX > 0) {
+        paddleX -= 7;
     }
     x += dx;
     y += dy;
