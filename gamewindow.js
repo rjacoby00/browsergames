f37fa939eee73a70e0550ed03a4804345b7a92bb
Ryan Jacoby <rjacoby00@icloud.com>
2017-12-05 12:23:58 -0800

    Update gamewindow.js

--- gamewindow.js
+++ gamewindow.js
@@ -7,6 +7,8 @@ var canvas = document.getElementById("rjgamesidrj");
 var ctx = canvas.getContext("2d");
 var x = canvas.width/2;
 var y = canvas.height-30;
+var dx = 2;
+var dy = -2;
 function drawBall() {
     ctx.beginPath();
     ctx.arc(x, y, 10, 0, Math.PI*2);
