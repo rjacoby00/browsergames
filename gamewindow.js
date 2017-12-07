cef9681782dc2acc6d6d2f994520cda53983af2e
Ryan Jacoby <rjacoby00@icloud.com>
2017-12-07 11:37:18 -0800

    Update gamewindow.js

--- gamewindow.js
+++ gamewindow.js
@@ -30,7 +30,7 @@ var brickOffsetLeft = 30;
 var bricks = [];
 for(var c=0; c<brickColumnCount; c++) {
     bricks[c] = [];
-    for(r=0; r<brickRowCount; r++) {
+    for(var r=0; r<brickRowCount; r++) {
         bricks[c][r] = { x: 0, y: 0 };
     }
 }
@@ -77,8 +77,8 @@ function drawPaddle() {
     ctx.closePath();
 }
 function drawBricks() {
-    for(c=0; c<brickColumnCount; c++) {
-        for(r=0; r<brickRowCount; r++) {
+    for(var c=0; c<brickColumnCount; c++) {
+        for(var r=0; r<brickRowCount; r++) {
             var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
             var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
             bricks[c][r].x = brickX;
